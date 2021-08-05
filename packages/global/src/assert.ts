afeb3d632f1e6936daeae326f1000eeb6e01e4eb
Mehrdad Reshadi <mehrdad@reshadi.com>
2021-08-05 00:35:48 -0700

    the package for global utilities such as assert

---
+++ packages/global/src/assert.ts
@@ -0,0 +1,26 @@
+
+interface Options {
+  getCallStack?: (skipFrameCount: number, exception?: any) => string[];
+  logger: {
+    error(message?: any, ...optionalParams: any[]): void;
+  };
+}
+
+const devOptions: Required<Options> = {
+  getCallStack: () => [],
+  logger: console,
+}
+
+export function assert(condition: boolean, message: string, options?: Options): void {
+  if (!condition) {
+    const callStackGetter = options?.getCallStack ?? devOptions.getCallStack;
+    const logger = options?.logger ?? devOptions.logger;
+    const callStack = callStackGetter(2);
+
+    if (callStack && callStack.length > 0) {
+      logger.error(message, callStack);
+    } else {
+      logger.error(message);
+    }
+  }
+}
---
+++ packages/global/test/assert.test.ts
@@ -0,0 +1,11 @@
+import "jest";
+import { assert } from "../src/assert";
+
+describe("test assert", () => {
+  test("test assert message", () => {
+    let _message;
+    assert(false, "test message", { logger: { error: message => { _message = message } } });
+    expect(_message).toBe("test message");
+  });
+
+})
