25178a7ef430db139b4c987bbb88694bde83f702
Mehrdad Reshadi <mehrdad@reshadi.com>
2021-12-13 16:50:46 -0800

    Updated package.json of each package to only list what it needs separately and moved all the shared packages for development to the top level package.json so we don't have to install each separately.

---
+++ packages/global/src/global.js
@@ -0,0 +1,9 @@
+import "./reference";
+if (typeof global === "object"
+    && typeof window !== "object"
+    && typeof __DEV__ !== "boolean") {
+    if (typeof global["__DEV__"] !== "boolean") {
+        global["__DEV__"] = false;
+    }
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2xvYmFsLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiZ2xvYmFsLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUNBLE9BQU8sYUFBYSxDQUFDO0FBSXJCLElBQ0UsT0FBTyxNQUFNLEtBQUssUUFBUTtPQUN2QixPQUFPLE1BQU0sS0FBSyxRQUFRO09BQzFCLE9BQU8sT0FBTyxLQUFLLFNBQVMsRUFDL0I7SUFDQSxJQUFJLE9BQU8sTUFBTSxDQUFDLFNBQVMsQ0FBQyxLQUFLLFNBQVMsRUFBRTtRQUMxQyxNQUFNLENBQUMsU0FBUyxDQUFDLEdBQUcsS0FBSyxDQUFDO0tBQzNCO0NBQ0YifQ==
---
+++ packages/global/src/reference.js
@@ -0,0 +1,3 @@
+"use strict";
+/// <reference path="./global.d.ts" />
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicmVmZXJlbmNlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsicmVmZXJlbmNlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxzQ0FBc0MifQ==
