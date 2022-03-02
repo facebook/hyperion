5a4755745a1b8ec04fc934e9032810cb6565671e
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-03-01 22:28:18 -0800

    added hyperion-util to the package, and also updated the version of dependencies to the latest

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // Copyright (c) Meta Platforms, Inc. and its affiliates.
 export * as INode from "@hyperion/hyperion-dom/src/INode";
 export * as IElement from "@hyperion/hyperion-dom/src/IElement";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSx5REFBeUQ7QUFFekQsT0FBTyxLQUFLLEtBQUssTUFBTSxrQ0FBa0MsQ0FBQztBQUMxRCxPQUFPLEtBQUssUUFBUSxNQUFNLHFDQUFxQyxDQUFDIn0=
+export * as SyncMutationObserver from "@hyperion/hyperion-util/src/SyncMutationObserver";
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSx5REFBeUQ7QUFFekQsT0FBTyxLQUFLLEtBQUssTUFBTSxrQ0FBa0MsQ0FBQztBQUMxRCxPQUFPLEtBQUssUUFBUSxNQUFNLHFDQUFxQyxDQUFDO0FBQ2hFLE9BQU8sS0FBSyxvQkFBb0IsTUFBTSxrREFBa0QsQ0FBQyJ9
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 
 export * as INode from "@hyperion/hyperion-dom/src/INode";
 export * as IElement from "@hyperion/hyperion-dom/src/IElement";
+export * as SyncMutationObserver from "@hyperion/hyperion-util/src/SyncMutationObserver";
