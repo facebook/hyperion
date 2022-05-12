14b2e1af5d8115791e62900cdc042bf2a44579c4
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-05-11 18:19:43 -0700

    Intercept event handlers on Window
    Added interceptors for global functions, constructors, or event handlers that
    support a callback or listener.

--- packages/hyperion-dom/src/IWindow.ts
+++ packages/hyperion-dom/src/IWindow.ts
@@ -5,9 +5,25 @@
 import { ConstructorInterceptor } from "@hyperion/hyperion-core/src/ConstructorInterceptor";
 import { FunctionInterceptor } from "@hyperion/hyperion-core/src/FunctionInterceptor";
 import { DOMShadowPrototype } from "./DOMShadowPrototype";
+import { EventHandlerAttributeInterceptor } from "./EventHandlerAttributeInterceptor";
 import { IEventTargetPrototype } from "./IEventTarget";
 
 export const IWindowPrototype = new DOMShadowPrototype(Window, IEventTargetPrototype, { sampleObject: window, registerOnPrototype: true });
 
 export const fetch = new FunctionInterceptor("fetch", IWindowPrototype);
+export const requestAnimationFrame = new FunctionInterceptor("requestAnimationFrame", IWindowPrototype);
+export const requestIdleCallback = new FunctionInterceptor("requestIdleCallback", IWindowPrototype);
+export const setInterval = new FunctionInterceptor("setInterval", IWindowPrototype);
+export const setTimeout = new FunctionInterceptor("setTimeout", IWindowPrototype);
+export const IntersectionObserver = new ConstructorInterceptor("IntersectionObserver", IWindowPrototype);
+export const MutationObserver = new ConstructorInterceptor("MutationObserver", IWindowPrototype);
 export const XMLHttpRequest = new ConstructorInterceptor("XMLHttpRequest", IWindowPrototype);
+
+
+//#region Event Handlers https://developer.mozilla.org/en-US/docs/Web/API/Window#event_handlers
+export const onerror = new EventHandlerAttributeInterceptor("onerror", IWindowPrototype);
+
+export const ondevicemotion = new EventHandlerAttributeInterceptor("ondevicemotion", IWindowPrototype);
+export const ondeviceorientation = new EventHandlerAttributeInterceptor("ondeviceorientation", IWindowPrototype);
+export const onorientationchange = new EventHandlerAttributeInterceptor("onorientationchange", IWindowPrototype);
+//#endretion
