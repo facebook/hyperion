721710b3d137e734a02397956137b63cb0c8a563
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-05-11 22:03:55 -0700

    Export new flowlet features into final build
    Rleaseing first version of Flowlet to the final build to be used by
    external apps.

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,6 @@ export * as SyncMutationObserver from "@hyperion/hyperion-util/src/SyncMutationO
 export { trackElementsWithAttributes } from "@hyperion/hyperion-util/src/trackElementsWithAttributes";
 export { onNetworkRequest } from "@hyperion/hyperion-util/src/onNetworkRequest";
 export { getVirtualPropertyValue, setVirtualPropertyValue } from "@hyperion/hyperion-core/src/intercept";
+export { initFlowletTrackers } from "@hyperion/hyperion-flowlet/src/Index";
+export { Flowlet } from "@hyperion/hyperion-flowlet/src/Flowlet";
+export { FlowletManager } from "@hyperion/hyperion-flowlet/src/FlowletManager";
