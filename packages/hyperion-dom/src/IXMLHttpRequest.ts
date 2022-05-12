f258b4f8f79ada3785ad1d56b0e7df6a27c83f09
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-05-11 18:21:28 -0700

    Intercept events of XHR
    added proper interceptors for various callback and event handlers on
    XHR.

--- packages/hyperion-dom/src/IXMLHttpRequest.ts
+++ packages/hyperion-dom/src/IXMLHttpRequest.ts
@@ -5,6 +5,7 @@
 import { AttributeInterceptor } from "@hyperion/hyperion-core/src/AttributeInterceptor";
 import { FunctionInterceptor } from "@hyperion/hyperion-core/src/FunctionInterceptor";
 import { DOMShadowPrototype } from "./DOMShadowPrototype";
+import { EventHandlerAttributeInterceptor } from "./EventHandlerAttributeInterceptor";
 import { IEventTargetPrototype } from "./IEventTarget";
 
 export const IXMLHttpRequestPrototype = new DOMShadowPrototype(XMLHttpRequest, IEventTargetPrototype, { sampleObject: new XMLHttpRequest(), registerOnPrototype: true });
@@ -12,3 +13,11 @@ export const IXMLHttpRequestPrototype = new DOMShadowPrototype(XMLHttpRequest, I
 export const open = new FunctionInterceptor("open", IXMLHttpRequestPrototype);
 export const send = new FunctionInterceptor("send", IXMLHttpRequestPrototype);
 export const withCredentials = new AttributeInterceptor("withCredentials", IXMLHttpRequestPrototype);
+
+export const onabort = new EventHandlerAttributeInterceptor("onabort", IXMLHttpRequestPrototype);
+export const onerror = new EventHandlerAttributeInterceptor("onerror", IXMLHttpRequestPrototype);
+export const onload = new EventHandlerAttributeInterceptor("onload", IXMLHttpRequestPrototype);
+export const onloadend = new EventHandlerAttributeInterceptor("onloadend", IXMLHttpRequestPrototype);
+export const onloadstart = new EventHandlerAttributeInterceptor("onloadstart", IXMLHttpRequestPrototype);
+export const onprogress = new EventHandlerAttributeInterceptor("onprogress", IXMLHttpRequestPrototype);
+export const ontimeout = new EventHandlerAttributeInterceptor("ontimeout", IXMLHttpRequestPrototype);
