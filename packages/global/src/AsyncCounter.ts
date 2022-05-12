e1d849c2d6e9a878c16ca1028d3e74c5bbfadc06
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-05-11 21:15:46 -0700

    enhance AsyncCounter
    expose current count of the object. Alos count how many steps it took to
    reach the target number.
    
    (useful for complex async tests)

--- packages/global/src/AsyncCounter.ts
+++ packages/global/src/AsyncCounter.ts
@@ -4,6 +4,7 @@
 
 export class AsyncCounter {
   private count: number = 0;
+  private steps: number = 0;
   private resolve!: (count: number) => void;
   private readonly promise;
 
@@ -13,16 +14,31 @@ export class AsyncCounter {
     });
   }
 
-  countUp(count = 1): this {
-    this.count += count;
+
+  getCount(): number {
+    return this.count;
+  }
+
+  getSteps(): number {
+    return this.steps;
+  }
+
+  private tryResolve() {
     if (this.count === this.targetCount) {
       this.resolve(this.count);
     }
+  }
+  countUp(count = 1): this {
+    this.count += count;
+    ++this.steps;
+    this.tryResolve();
     return this;
   }
 
   countDown(count = 1): this {
     this.count -= count;
+    ++this.steps;
+    this.tryResolve();
     return this;
   }
 
