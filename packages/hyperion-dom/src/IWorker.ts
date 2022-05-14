ce257181580d1ab72db7098c74167a6b130c1c99
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-05-13 20:29:12 -0700

    Enable worker interception without breaking jest
    Worker is not defined in the jest environment. Added extra logic to
    ensure looking up the Worker constructor does not cracsh jest

--- packages/hyperion-dom/src/IWorker.ts
+++ packages/hyperion-dom/src/IWorker.ts
@@ -6,7 +6,12 @@ import { DOMShadowPrototype } from "./DOMShadowPrototype";
 import { EventHandlerAttributeInterceptor } from "./EventHandlerAttributeInterceptor";
 import { IEventTargetPrototype } from "./IEventTarget";
 
-export const IWorkerPrototype = new DOMShadowPrototype(Worker, IEventTargetPrototype, { registerOnPrototype: true });
+/**
+ * In jest environment Worker is not defined. The following will allow tests to run
+ * while actually keeping the feature enabled for browsers. 
+ */
+const WorkerClass = window.Worker ?? { prototype: <Worker><unknown>IEventTargetPrototype.targetPrototype, }
+export const IWorkerPrototype = new DOMShadowPrototype<Worker, EventTarget>(WorkerClass, IEventTargetPrototype, { registerOnPrototype: true });
 
 export const onmessage = new EventHandlerAttributeInterceptor("onmessage", IWorkerPrototype);
 export const onmessageerror = new EventHandlerAttributeInterceptor("onmessageerror", IWorkerPrototype);
