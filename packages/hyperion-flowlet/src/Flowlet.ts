453501e3212f2e6bb22c60b996450ad5df9d5d79
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-06-08 01:11:50 -0700

    Enable Flowlet to carry data With this change, chain of flowlets can carry data. This effectively creates a capability similar to variable closure for asyn flows.

--- packages/hyperion-flowlet/src/Flowlet.ts
+++ packages/hyperion-flowlet/src/Flowlet.ts
@@ -2,20 +2,22 @@
  * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
  */
 
-export class Flowlet {
+export class Flowlet<T extends {} = {}> {
+  readonly data: T;
   private readonly _fullName: string;
   constructor(
     public readonly name: string,
-    public readonly parent?: Flowlet
+    public readonly parent?: Flowlet<T>
   ) {
     this._fullName = `${this.parent?.fullName() ?? ""}/${this.name}`;
+    this.data = Object.create(parent?.data ?? null);
   }
 
   fullName(): string {
     return this._fullName;
   }
 
-  fork(name: string): Flowlet {
-    return new Flowlet(name, this);
+  fork(name: string): Flowlet<T> {
+    return new Flowlet<T>(name, this);
   }
 }
---
+++ packages/hyperion-flowlet/test/Flowlet.test.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+ */
+
+import "jest";
+import { Flowlet } from "../src/Flowlet";
+
+describe("test Flowlet", () => {
+  test("test Flowlet methods", () => {
+    const f1 = new Flowlet<{
+      i?: number;
+    }>("f1");
+
+    const f2 = f1.fork("f2");
+
+    expect(f2.parent).toStrictEqual(f1);
+    expect(f1.fullName()).toBe("/f1");
+    expect(f2.fullName()).toBe("/f1/f2");
+    f1.data.i = 10;
+    expect(f2.data.i).toBe(10);
+  });
+
+});
