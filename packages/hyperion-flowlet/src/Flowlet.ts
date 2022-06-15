309a7eac41d8e576b6ddc533c252b3066f0a0217
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-06-15 16:52:13 -0700

    Convert Flowlet.fullName() function to Flowlet.fullName property
    We already calculate the `fullName` as a readonly property in the
    `Flowlet`. There is no point pay for the overhead of function call on
    top of that to access the field. This makes the class a bit simpler.
    
    a Please enter the commit message for your changes. Lines starting

--- packages/hyperion-flowlet/src/Flowlet.ts
+++ packages/hyperion-flowlet/src/Flowlet.ts
@@ -4,17 +4,13 @@
 
 export class Flowlet<T extends {} = {}> {
   readonly data: T;
-  private readonly _fullName: string;
+  readonly fullName: string;
   constructor(
     public readonly name: string,
     public readonly parent?: Flowlet<T>
   ) {
-    this._fullName = `${this.parent?.fullName() ?? ""}/${this.name}`;
+    this.fullName = `${this.parent?.fullName ?? ""}/${this.name}`;
     this.data = Object.create(parent?.data ?? null);
-  }
-
-  fullName(): string {
-    return this._fullName;
   }
 
   fork(name: string): Flowlet<T> {
