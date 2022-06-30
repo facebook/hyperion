0f32c77489744f25ea944c13996d80d5782cc216
Mehrdad Reshadi <mehrdad@reshadi.com>
2022-06-30 12:06:05 -0700

    Removed *.js files from the repo.
    We will ignore all the .js files in the git. Originally I kept them
    there in case anyone wanted to just import the repo and work with it.
    But it is a pain to keep track of.
    If/when we wanted to post these packages to npm, we can choose to
    include the .js files then.
    And anyone who pulls in the code, can easily build it now.

--- packages/global/src/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
- */
-export * from "./assert";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7R0FFRztBQUVILGNBQWMsVUFBVSxDQUFDIn0=
--- packages/hyperion-dom/src/IDocument.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
- */
-import { FunctionInterceptor } from "@hyperion/hyperion-core/src/FunctionInterceptor";
-import { DOMShadowPrototype } from "./DOMShadowPrototype";
-import { INodePrototype } from "./INode";
-export const IDocumentPrototype = new DOMShadowPrototype(Document, INodePrototype, { nodeType: window.document.DOCUMENT_NODE });
-export const createElement = new FunctionInterceptor("createElement", IDocumentPrototype);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSURvY3VtZW50LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiSURvY3VtZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOztHQUVHO0FBRUgsT0FBTyxFQUFFLG1CQUFtQixFQUFFLE1BQU0saURBQWlELENBQUM7QUFDdEYsT0FBTyxFQUFFLGtCQUFrQixFQUFFLE1BQU0sc0JBQXNCLENBQUM7QUFDMUQsT0FBTyxFQUFFLGNBQWMsRUFBRSxNQUFNLFNBQVMsQ0FBQztBQUV6QyxNQUFNLENBQUMsTUFBTSxrQkFBa0IsR0FBRyxJQUFJLGtCQUFrQixDQUFDLFFBQVEsRUFBRSxjQUFjLEVBQUUsRUFBRSxRQUFRLEVBQUUsTUFBTSxDQUFDLFFBQVEsQ0FBQyxhQUFhLEVBQUUsQ0FBQyxDQUFDO0FBRWhJLE1BQU0sQ0FBQyxNQUFNLGFBQWEsR0FBRyxJQUFJLG1CQUFtQixDQUFDLGVBQWUsRUFBRSxrQkFBa0IsQ0FBQyxDQUFDIn0=
