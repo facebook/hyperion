f2f410ea4c155e877f13fa23a9b5893d3480e2ee
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-01-17 23:08:17 -0800

    Fixed global variable detection
    Fixed the logic to work accros browser and nodejs environment correctly

--- packages/global/src/global.ts
+++ packages/global/src/global.ts
@@ -4,25 +4,19 @@
 
 import "./reference";
 
-declare var window: Window;
-declare var global: Object & {
+type OurGlobal = {
   __DEV__?: boolean;
+
+}
+
+type NodeGlobal = {
   process?: {
     env?: { [index: string]: any }
   }
 };
 
-if (
-  typeof global === "object"
-  && typeof __DEV__ !== "boolean"
-) {
-  if (
-    global?.process?.env?.JEST_WORKER_ID ||
-    global?.process?.env?.NODE_ENV === 'development'
-  ) {
-    global["__DEV__"] = true;
-  }
-}
+declare var window: Window;
+declare var global: Object & OurGlobal & NodeGlobal;
 
 const globalScope =
   typeof globalThis === "object" ? globalThis :
@@ -30,5 +24,20 @@ const globalScope =
       typeof window === "object" ? window :
         typeof self === "object" ? self :
           {};
+
+
+if (typeof __DEV__ !== "boolean") {
+  (globalThis as OurGlobal).__DEV__ = true;
+
+  if (
+    typeof global === "object" && (
+      global?.process?.env?.JEST_WORKER_ID ||
+      global?.process?.env?.NODE_ENV === 'development'
+    )
+  ) {
+    global["__DEV__"] = true;
+  }
+}
+
 
 export default globalScope;
