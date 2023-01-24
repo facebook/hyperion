fc5e5b8880993593ccf5886b22c2b0aac86b1383
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-01-24 11:56:55 -0800

    Improve the types of visitors
    Used couple type mapper and type algebra to improve the types of the
    visitor functions.

--- packages/hyperion-react/src/FlowToTsTypes.ts
+++ packages/hyperion-react/src/FlowToTsTypes.ts
@@ -1,2 +1,3 @@
 export interface Class<T, A extends any[] = any[]> extends Function { new(...args: A): T; }
-export type mixed = unknown;
+export type mixed = any;//unknown;
+export type $Values<T> = T[keyof T];
