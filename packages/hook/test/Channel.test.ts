f2813f6da18968319a1d5c7fe5905db9d3806a95
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-01-31 23:42:38 -0800

    Added a Channel to support multi-hooks for callbacks.
    If we have too many events to fire, it is not efficient to always create
    emepty Hooks for them.
    The Channel class allows a typed mechanism to lazy create the Hookx only
    when there is a callback registered for it.
    
    This mechanism will allow one module to expose multipe events at once.
    
    Not that this is slightly slower than having dedicated hooks since it
    involves a lookup and a check.

---
+++ packages/hook/test/Channel.test.ts
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+ */
+
+import "jest";
+import { Channel } from "../src/Channel";
+
+type ChannelEvents = {
+  ev1: [],
+  ev2: [i: number],
+  ev3: [i: number, s: string]
+}
+
+describe("test Channel", () => {
+  test("empty callback", () => {
+    const channel = new Channel<Pick<ChannelEvents, 'ev1'>>();
+    expect(channel.emit('ev1')).toBe(undefined);
+  });
+
+  test("simple callback", () => {
+    const channel = new Channel<ChannelEvents>();
+    const fn1 = jest.fn((i: number) => { });
+    const fn2 = jest.fn<void, ChannelEvents['ev1']>();
+
+    channel.on('ev2').add(fn1);
+    channel.on('ev1').add(fn2);
+
+    channel.emit('ev2', 20);
+    expect(fn2).toBeCalledTimes(0);
+    expect(fn1).toBeCalledTimes(1);
+    expect(fn1.mock.calls[0]).toEqual([20]);
+  });
+
+  test("multiple callbacks", () => {
+    const channel = new Channel<ChannelEvents>();
+    const fn1 = jest.fn<void, ChannelEvents['ev3']>();
+    const fn2 = jest.fn<void, ChannelEvents['ev3']>();
+
+    channel.on('ev3').add(fn1);
+    channel.on('ev3').add(fn2);
+
+    channel.emit('ev3', 20, "hi");
+
+    expect(fn1).toBeCalledTimes(1);
+    expect(fn1.mock.calls[0]).toEqual([20, "hi"]);
+
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2.mock.calls[0]).toEqual([20, "hi"]);
+  });
+
+});
