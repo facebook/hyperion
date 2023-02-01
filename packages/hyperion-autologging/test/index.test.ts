93109e9a5eaf9acca558b90cbe3034fb58802f7d
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-01-31 16:24:35 -0800

    Added a dummy test for AutoLogging package
    Still don't have any unit test for the hyperion-autologging package, but
    need some empty file to make sure the whole repo's tests passes.

---
+++ packages/hyperion-autologging/test/index.test.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+ */
+
+
+describe('autologging', () => {
+  it('test index', () => {
+    // expect(sum(1, 1)).toEqual(2);
+  });
+});
