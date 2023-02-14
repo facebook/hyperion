0a741a584d7a80d5d3cf2e4ba9b48cadad9000db
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-02-14 12:05:23 -0800

    Remove type parameters from as many init functions as possible.
    Since TS is more capable in terms of checking sub-types, we no longer
    need to have all types passed type parameters to functions (this was
    needed in Flowjs).
    
    By removing the type parameters, we can simply enforce the type
    requirements with some interfaces. This means, we can better modularize
    the code and don't need to wrap everything in init functions.
    
    This commit specially set the stage for fixing the portal issue next.

---
+++ packages/hyperion-autologging/src/ALFlowletManager.ts
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+ */
+
+import { FlowletManager as BaseFlowletManager } from "@hyperion/hyperion-flowlet/src/FlowletManager";
+import { Flowlet } from "@hyperion/hyperion-flowlet/src/Flowlet";
+
+
+
+/**
+ * We want to allow type of flowlet to be passed here. the only thing we need
+ * from the data type is the `surface` field. Sonce, ALFlowlet may change
+ * overtime, we don't want to create an uncessary dependency on that.
+ */
+export interface ALFlowletDataType {
+  surface?: string,
+};
+
+class ALFlowlet<DataType extends ALFlowletDataType> extends Flowlet<DataType>{
+
+}
+
+export class ALFlowletManager<DataType extends ALFlowletDataType> extends BaseFlowletManager<ALFlowlet<DataType>>{
+  constructor() {
+    super(ALFlowlet);
+  }
+}
+
