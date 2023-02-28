2399ced644c3be81afcd4876843905f7be1ffb3d
Belton Zhong <beltonzhong@fb.com>
2023-02-28 15:21:59 -0800

    Clean up code

--- packages/hyperion-autologging/src/ALSurfaceUtils.ts
+++ packages/hyperion-autologging/src/ALSurfaceUtils.ts
@@ -3,17 +3,9 @@ import { AUTO_LOGGING_SURFACE } from "./ALSurfaceConsts";
 const SURFACE_SELECTOR = `[${AUTO_LOGGING_SURFACE}]`;
 
 export function getSurfacePath(node: HTMLElement | null): string| null {
-  const surfacePath = getAncestralSurfaceNode(node)?.getAttribute(AUTO_LOGGING_SURFACE);
-  if (surfacePath != undefined) {
-    return surfacePath;
-  }
-  return null;
+  return getAncestralSurfaceNode(node)?.getAttribute(AUTO_LOGGING_SURFACE) ?? null;
 }
 
 export function getAncestralSurfaceNode(node: Element| null): Element| null {
-  const surfaceNode = node?.closest(SURFACE_SELECTOR);
-  if (surfaceNode != undefined) {
-    return surfaceNode;
-  }
-  return null;
+  return node?.closest(SURFACE_SELECTOR) ?? null;
 }
