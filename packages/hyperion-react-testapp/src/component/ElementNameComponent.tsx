eb476c9a2a9d154c312e6a87cd2ceee0d15af0a7
Belton Zhong <beltonzhong@fb.com>
2023-07-28 15:48:23 -0700

    improve test clarity

--- packages/hyperion-react-testapp/src/component/ElementNameComponent.tsx
+++ packages/hyperion-react-testapp/src/component/ElementNameComponent.tsx
@@ -1,39 +1,57 @@
 export default function (/* props: Props */) {
-
+  function setExpectedElementName(name: string): void {
+    const element = document.getElementById('expectedElementName');
+    if(element != null) {
+      element.innerHTML = name;
+    }
+  }
   return(
     <div>
-      <table border={1}>
+      <h3>Element Name Testing</h3>
+      Check the console for al_ui_events after clicking the buttons. <br />
+      Expected elementName: <div id="expectedElementName"></div>
+      <table border={1} style={{
+        margin: '0 auto',
+      }}>
         <tbody>
           <tr>
             <td>aria-label</td>
             <td>
-              <button aria-label="Test Label 1">&nbsp;</button>
+              <button onClick={() => {
+                setExpectedElementName('Test Label 1');
+              }} aria-label="Test Label 1">&nbsp;</button>
             </td>
           </tr>
           <tr>
             <td>aria-labelledby</td>
             <td>
-              <button aria-labelledby="label2 label3">&nbsp;</button>
+              <button onClick={() => {
+                setExpectedElementName('Test Label 2 Another Label');
+              }} aria-labelledby="label2 label3">&nbsp;</button>
             </td>
           </tr>
           <tr>
             <td>aria-description</td>
             <td>
-              <button aria-description="Test Description 1">&nbsp;</button>
+              <button onClick={() => {
+                setExpectedElementName('Test Description 1');
+              }} aria-description="Test Description 1">&nbsp;</button>
             </td>
           </tr>
           <tr>
             <td>aria-describedby</td>
             <td>
-              <button aria-describedby="desc2 desc3">&nbsp;</button>
+              <button onClick={() => {
+                setExpectedElementName('Test Description 2 Another Description');
+              }}aria-describedby="desc2 desc3">&nbsp;</button>
             </td>
           </tr>
         </tbody>
       </table>
-      <div id="label2">Test Label 2</div>
-      <div id="label3">Another Label</div>
-      <div id="desc2">Test Description 2</div>
-      <div id="desc3">Another Description </div>
+      <div style={{display: 'none'}} id="label2">Test Label 2</div>
+      <div style={{display: 'none'}} id="label3">Another Label</div>
+      <div style={{display: 'none'}} id="desc2">Test Description 2</div>
+      <div style={{display: 'none'}} id="desc3">Another Description</div>
     </div>
   );
 }
