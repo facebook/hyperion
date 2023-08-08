7f4a62c73fef9dd14ff121c9f023046c7cd6fdc7
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-08-07 22:10:07 -0700

    Update text extraction algorith to walk downward.
    Instead of moving up the dom tree to look for a suitable element text,
    the new algorithm:
    * First goes up the tree from event.traget until it finds an
      interactable element
    * Then it recursively looks at the sub-tree and uses `aria-*` or
      `textContext` of the nodes to construct the final text.
    
    There are two new callback in the options that one can use to control
    individual values before they are merged, or the final value while it is
    beting merged.

---
+++ packages/hyperion-autologging/test/ALInteractableDOMElement.test.ts
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+ *
+ * @jest-environment jsdom
+ */
+
+import "jest";
+
+import * as ALInteractableDOMElement from "../src/ALInteractableDOMElement";
+
+function html(text: string): HTMLElement {
+  const element = document.createElement("div");
+  element.innerHTML = text;
+  document.body.appendChild(element); // To ensure query functions of document works
+  return element;
+}
+
+function createTestDom(): HTMLElement {
+  return html(`
+  <span id='1' aria-label="test1"></span>
+  <span id='2' aria-description="test2"></span>
+  <span id='3'>test3</span>
+  <span id='4' aria-labelledby="3"></span>
+  <span id='5' aria-describedby="3"></span>
+  <span id='6' aria-label="test6" aria-labelledby="3 8"></span>
+  <span id='7' aria-description="test7" aria-describedby="8"></span>
+  <span id='8'>test8</span>
+  <span id='9' aria-label="test9">ignored</span>
+  <span id='10'><span aria-label="te"></span>s<span>t10</span></span>
+  `);
+}
+
+function getText(id: string): string | null {
+  const element = document.getElementById(id);
+  return ALInteractableDOMElement.getElementTextEvent(element, null).elementName;
+}
+
+describe("Text various element text options", () => {
+  test("element with simple text", () => {
+    createTestDom();
+
+    expect(getText(`1`)).toBe("test1");
+    expect(getText(`2`)).toBe("test2");
+    expect(getText(`3`)).toBe("test3");
+    expect(getText(`4`)).toBe("test3");
+    expect(getText(`5`)).toBe("test3");
+    expect(getText(`6`)).toBe("test3test8");
+    expect(getText(`7`)).toBe("test7");
+    expect(getText(`8`)).toBe("test8");
+    expect(getText(`9`)).toBe("test9");
+    expect(getText(`10`)).toBe("test10");
+  });
+
+  test("element text callbacks", () => {
+    const dom = createTestDom();
+    interface ExtendedElementText extends ALInteractableDOMElement.ALElementText {
+      isModified?: boolean;
+      modifiedText?: string;
+    }
+
+    ALInteractableDOMElement.init({
+      updateText(elementText: ExtendedElementText, domSource) {
+        const { text } = elementText;
+        if (/test[8-9]/.test(text)) {
+          elementText.isModified = true;
+          elementText.text = text.replace(/(\w+)([0-9]+)(\w*)/, "$1*$3");
+          elementText.modifiedText = text;
+          console.log(text, elementText);
+        }
+        elementText.text = elementText.text.replace(/\r\n|\r|\n/, '');
+      },
+      reduceText: (prev: ExtendedElementText, current: ExtendedElementText, next: ExtendedElementText) => {
+        next.modifiedText = (prev.modifiedText ?? "") + (current.modifiedText ?? "");
+      }
+    });
+    const text = ALInteractableDOMElement.getElementTextEvent(dom, null);
+    expect(text.elementName).toBe("  test1  test2  test3  test3  test3  test3test*  test7  test*  test*  test10  ");
+    console.log(text);
+
+  });
+});
