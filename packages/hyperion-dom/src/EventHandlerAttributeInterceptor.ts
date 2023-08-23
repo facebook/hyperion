b0856185443a7c045e3369b42227101496ac8805
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-08-22 21:09:24 -0700

    handle on* event handlers
    Enabled handling of `element.on* = (...)=>{ ... }`  both for wrapping the callback and passing the right flowlet, and for detecting interactable elements.

--- packages/hyperion-dom/src/EventHandlerAttributeInterceptor.ts
+++ packages/hyperion-dom/src/EventHandlerAttributeInterceptor.ts
@@ -2,23 +2,54 @@
  * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
  */
 
-import { AttributeInterceptor, getAttributeInterceptor } from "@hyperion/hyperion-core/src/AttributeInterceptor";
+import { AttributeInterceptor, interceptAttributeBase } from "@hyperion/hyperion-core/src/AttributeInterceptor";
 import { ShadowPrototype } from "@hyperion/hyperion-core/src/ShadowPrototype";
+import { ElementAttributeInterceptor } from "./ElementAttributeInterceptor";
+
 
 class EventHandlerAttributeInterceptor<
   BaseType extends EventTarget & { [key: string]: any },
   Name extends string,
-  > extends AttributeInterceptor<BaseType, Name> {
+> extends AttributeInterceptor<BaseType, Name> {
 
 }
 
+/**
+ * Use this function when even even handler cannot be also added via
+ * a setAttribute or Attr.value on the element.
+ * This usually applies to non-element nodes.
+ */
 export function interceptEventHandlerAttribute<
   BaseType extends EventTarget & { [key: string]: any },
   Name extends string,
-  >(
-    name: Name,
-    shadowPrototype: ShadowPrototype<BaseType>
-  ): EventHandlerAttributeInterceptor<BaseType, Name> {
-  const desc = getAttributeInterceptor<BaseType, Name, BaseType[Name], BaseType[Name], EventHandlerAttributeInterceptor<BaseType, Name>>(name, shadowPrototype);
-  return desc?.interceptor ?? new EventHandlerAttributeInterceptor(name, shadowPrototype, desc);
+>(
+  name: Name,
+  shadowPrototype: ShadowPrototype<BaseType>
+): EventHandlerAttributeInterceptor<BaseType, Name> {
+  return interceptAttributeBase<BaseType, Name, BaseType[Name], BaseType[Name], EventHandlerAttributeInterceptor<BaseType, Name>>(name, shadowPrototype, EventHandlerAttributeInterceptor);
 }
+
+class ElementEventHandlerAttributeInterceptor<
+  BaseType extends Element & { [key: string]: any },
+  Name extends string,
+> extends ElementAttributeInterceptor<BaseType, Name, BaseType[Name], BaseType[Name]> {
+
+}
+
+/**
+ * Use this function when the event handler can be set via node.onX
+ * as well as node.setAttribute("X", ....) or node.getAttributeNode("X").value = ...
+ * This mostly applies to Elements. 
+ * In these cases, one need to cover both raw (text) and processed (function) values
+ * for the handler. 
+ */
+export function interceptElementEventHandlerAttribute<
+  BaseType extends Element & { [key: string]: any },
+  Name extends string,
+>(
+  name: Name,
+  shadowPrototype: ShadowPrototype<BaseType>
+): ElementEventHandlerAttributeInterceptor<BaseType, Name> {
+  return interceptAttributeBase<BaseType, Name, BaseType[Name], BaseType[Name], ElementEventHandlerAttributeInterceptor<BaseType, Name>>(name, shadowPrototype, ElementEventHandlerAttributeInterceptor);
+}
+
--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ export * as IEventTarget from "@hyperion/hyperion-dom/src/IEventTarget";
 export * as IElement from "@hyperion/hyperion-dom/src/IElement";
 export * as IHTMLElement from "@hyperion/hyperion-dom/src/IHTMLElement";
 export * as ICSSStyleDeclaration from "@hyperion/hyperion-dom/src/ICSSStyleDeclaration";
+// export * as IGlobalEventHandlers from "@hyperion/hyperion-dom/src/IGlobalEventHandlers";
 // export * as IWindow from "@hyperion/hyperion-dom/src/IWindow";
 // export * as IXMLHttpRequest from "@hyperion/hyperion-dom/src/IXMLHttpRequest";
 
