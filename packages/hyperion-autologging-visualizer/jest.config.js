6f037f2ed21403d6fb913a1256a1015a56f06519
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-08-31 15:35:42 -0700

    Added new package for visualizer
    setup a new package for the visualizer.

---
+++ packages/hyperion-autologging-visualizer/jest.config.js
@@ -0,0 +1,6 @@
+const baseConfig = require('@hyperion/devtools/jest.config');
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+module.exports = baseConfig;
