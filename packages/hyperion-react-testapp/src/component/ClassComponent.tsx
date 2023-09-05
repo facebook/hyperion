db2d20d023c83e10ce0de1a42cf376bd9fed8b37
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-09-05 10:58:19 -0700

    Use a new mechanism for reading surface context in class components
    For class components, we cannot use the `useContext` hook to read the
    context.
    We used to use an internal and unsafe mechanism to read the context
    value and get the surface.
    Instead in this change, we use the constructor interceptor of
    ClassComponent to read the context value.
    To be sure the right context is assigned, we use the legacy context
    mechanism (using `contextType`) to assign the surface context value to
    it.
    This is a best effort tosupport legacy components and does not guarantee
    the 100% coverage, but usually is good enough.
    
    Also added a new test component to the test react app to show case that
    the render is getting the right flowlet value in that component.

--- packages/hyperion-react-testapp/src/component/ClassComponent.tsx
+++ packages/hyperion-react-testapp/src/component/ClassComponent.tsx
@@ -5,18 +5,56 @@
 import React from "react";
 import FuncComponent from "./FuncComponent";
 import { Props, Surface } from "./Surface";
+import { ALSurfaceContext } from "@hyperion/hyperion-autologging/src/ALSurfaceContext";
+import { FlowletManager } from "../FlowletManager";
 
+class ClassCompWithSurface extends React.Component<{}>{
+  private foo() {
+    const flowlet = FlowletManager.top();
+    if (flowlet?.name !== "In Render") {
+      console.error("Invalid flowlet in this method", flowlet?.getFullName());
+    } else {
+      console.log("Recieved correct flowlet", flowlet.getFullName());
+    }
+  }
+  render(): React.ReactNode {
+    if (ALSurfaceContext) {
+      const outterFlowlet = FlowletManager.top();
+      return <ALSurfaceContext.Consumer>
+        {value => {
+          const surface = value.surface;
+          const surfaceFlowlet = value.flowlet;
+          const flowlet = FlowletManager.top();
+          const newFlowlet = FlowletManager.push(new FlowletManager.flowletCtor("In Render", surfaceFlowlet));
+          const result = <table border={1} ><tbody>
+            <tr><th>Surface</th><td>{surface}</td></tr>
+            <tr><th>Surface Flowlet: </th><td>{surfaceFlowlet?.getFullName()}</td></tr>
+            <tr><th>Inner Render Flowlet: </th><td>{flowlet?.getFullName()}</td></tr>
+            <tr><th>Outter Render Flowlet: </th><td>{outterFlowlet?.getFullName()}</td></tr>
+          </tbody></table>;
+          this.foo();
+          FlowletManager.pop(newFlowlet);
+          return result;
+        }
+        }
+      </ALSurfaceContext.Consumer>
+    }
+  }
+}
 export default class ClassComponent extends React.Component<Props> {
   render() {
     return Surface({ surface: `ClassComp: ${this.props.message}`, metadata: { type: 'class component' } })(
-      <ul data-comptype="class">
-        <li>The class component</li>
-        <li>{this.props.message}</li>
-        <li>
-          <FuncComponent message="func comp inside class compo" />
-          {this.props.children}
-        </li>
-      </ul>,
+      <>
+        <ul data-comptype="class">
+          <li>The class component</li>
+          <li>{this.props.message}</li>
+          <li>
+            <FuncComponent message="func comp inside class compo" />
+            {this.props.children}
+          </li>
+        </ul>
+        <ClassCompWithSurface></ClassCompWithSurface>
+      </>,
     );
   }
 }
