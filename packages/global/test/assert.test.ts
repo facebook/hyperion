c23b7b0858428368f14eca755ee0c1a87190a533
Mehrdad Reshadi <mehrdad@reshadi.com>
2023-09-06 12:37:47 -0700

    Fix __DEV__ value.
    We need to ensure __DEV__ is always defined since a lot of the code
    directly uses it. We also should make sure this code runs ok in github
    as well as the envirinment into which the code is imported.
    
    Fixed the assignment to only update it to false in the JEST environment.

--- packages/global/test/assert.test.ts
+++ packages/global/test/assert.test.ts
@@ -20,5 +20,9 @@ describe("test assert", () => {
     assert(false, "test message");
     expect(fn).toBeCalledTimes(1);
     expect(fn.mock.calls[0][0]).toBe("test message");
-  })
+  });
+
+  test("Reading __DEV__ in test", () => {
+    expect(__DEV__).toBeDefined();
+  });
 })
