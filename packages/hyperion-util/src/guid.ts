a77be2ffe1cc805069fb6b6724270fa165f8a5eb
Vincent Lee <williewillus@users.noreply.github.com>
2023-12-11 16:10:24 -0600

    Use window.crypto.getRandomValues to generate guids
    Internal: See T164417895

--- packages/hyperion-util/src/guid.ts
+++ packages/hyperion-util/src/guid.ts
@@ -8,5 +8,5 @@
 export type GUID = string;
 
 export function guid(): GUID {
-  return 'f' + (Math.random() * (1 << 30)).toString(16).replace('.', '');
+  return 'f' + (window.crypto.getRandomValues(new Uint32Array(1))[0]).toString(16).replace('.', '');
 }
