ed07ee5a545a5356df5e1aec2d0eaa6d4985b392
Mehrdad Reshadi <mehrdad@reshadi.com>
2024-04-02 17:15:28 -0700

    Renamed devtools folder to hyperion-devtools
    Rename the package to enable us later publish to npm.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 // Copyright (c) Meta Platforms, Inc. and its affiliates.
 
-const baseConfig = require('@hyperion/devtools/babel.config');
+const baseConfig = require('@hyperion/hyperion-devtools/babel.config');
 module.exports = baseConfig;
