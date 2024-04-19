8f1007d6cb9e75dfd8c04d4ad8384187dc6fe5ec
Mehrdad Reshadi <mehrdad@reshadi.com>
2024-04-19 00:13:40 -0700

    Enabled multi-channel pipe as well as pausable channels.
    In order to allow more flexibility for the applications to setup their
    own custom setup, and to allo more than one set of subscribers be
    enabled, made the following changes:
    - Channel can now pipe to more than one channel. This way, we can have a
      core channel for main events, and mutliple application/features can
      make their own dedicated channels and add their own set of subscribers
      to those channels.
    - A PausableChannel can be paused/unpaused which allows us to drop
      events when needed. This mechanism would have been equivalend to
      unsubscribe all event handlers (pause), and then add them back
      (unpause) when needed.

--- packages/hyperion-channel/src/Channel.ts
+++ packages/hyperion-channel/src/Channel.ts
@@ -3,12 +3,12 @@
  */
 
 import { Hook } from "@hyperion/hyperion-hook/src/Hook";
-
-type Listener<V extends any[]> = (...args: V) => void;
-type Listeners<V extends any[]> = Hook<Listener<V>>;
-export type BaseChannelEventType = { [key: string]: any[] };
+type ListenerArgs = unknown[];
+type Listener<V extends ListenerArgs> = (...args: V) => void;
+type Listeners<V extends ListenerArgs> = Hook<Listener<V>>;
+export type BaseChannelEventType = { [key: string]: ListenerArgs; };
 type ChannelListeners<T extends BaseChannelEventType> = {
-  [P in keyof T]: Listeners<T[P]>
+  [P in keyof T]: Listeners<T[P]> | undefined;
 }
 
 export type ChannelEventType<ChannelType> = ChannelType extends Channel<infer EventType> ? EventType : never;
@@ -20,11 +20,47 @@ interface IEmitter<TEventToListenerArgsMap extends BaseChannelEventType> {
   >(eventType: TEvent, ...rawArgs: TArgs): void;
 }
 
-export class PipeableEmitter<TEventToListenerArgsMap extends BaseChannelEventType> implements IEmitter<TEventToListenerArgsMap> {
-  private _next: IEmitter<TEventToListenerArgsMap>['emit'] | null = null;
+interface IPipedEmitter<TEventToListenerArgsMap extends BaseChannelEventType> {
+  emit<
+    TEvent extends keyof TEventToListenerArgsMap,
+  >(eventType: TEvent, ...rawArgs: any): void;
+}
 
+
+/// Checks if two types are the same
+type IfEquals<T, U, Y = unknown, N = never> =
+  (<G>() => G extends T ? 1 : 2) extends
+  (<G>() => G extends U ? 1 : 2) ? Y : N;
+
+/// Returns the subset of Dest that has the same keys as Src
+type Sub<Dest, Src> = Omit<Dest, Exclude<keyof Dest, keyof Src>>;
+
+
+/// Extracts events of a give type
+type Events<T> =
+  T extends IPipedEmitter<infer E> ? E
+  : T extends IPipedEmitter<infer E> ? E
+  : T extends PipeableEmitter<infer E> ? E
+  : T extends Channel<infer E> ? E
+  : never;
+
+/// Checks strict equality of the events of T with BaseEvents. 
+type Check<T, BaseEvents> = IfEquals<Sub<Events<T>, BaseEvents>, BaseEvents, unknown, never>;
+
+export class PipeableEmitter<TEventToListenerArgsMap extends BaseChannelEventType> implements IEmitter<TEventToListenerArgsMap> {
+  private _next = new Hook<IEmitter<TEventToListenerArgsMap>['emit']>();
+
+  /**
+   * If the nextChannel is a super set of events of the current one, we still want to allow the
+   * pipe to work. This type overload enables that by using a less restrictive emit function.
+   * As long as events names are a subset of TEventToListenerArgsMap, it will be ok.
+   * The P parameter bellow also detects if the arguments of the event names exactly match. In rare cases that the nextChannel
+   * has redefined existing event types, this argument turn off the return type of this function. Note that, ideally we should
+   * create a compile time error, but I couldn't figure out how to do this with a single argument to the function.
+   */
+  pipe<T extends IPipedEmitter<TEventToListenerArgsMap>, P = Check<T, TEventToListenerArgsMap>>(nextChannel: T, scheduler?: (task: () => void) => void): T & P;
   pipe<T extends IEmitter<TEventToListenerArgsMap>>(nextChannel: T, scheduler?: (task: () => void) => void): T {
-    this._next = scheduler
+    this._next.add(scheduler
       ? (eventType, ...args) => {
         scheduler(() => {
           nextChannel.emit(eventType, ...args);
@@ -32,7 +68,8 @@ export class PipeableEmitter<TEventToListenerArgsMap extends BaseChannelEventTyp
       }
       : (eventType, ...args) => {
         nextChannel.emit(eventType, ...args);
-      };
+      }
+    );
     return nextChannel;
   }
 
@@ -40,7 +77,7 @@ export class PipeableEmitter<TEventToListenerArgsMap extends BaseChannelEventTyp
     TEvent extends keyof TEventToListenerArgsMap,
     TArgs extends TEventToListenerArgsMap[TEvent]
   >(eventType: TEvent, ...rawArgs: TArgs): void {
-    this._next?.(eventType, ...rawArgs);
+    this._next.call(eventType, ...rawArgs);
   }
 }
 
@@ -53,7 +90,7 @@ export class Channel<TEventToListenerArgsMap extends BaseChannelEventType>
   private _getOrAddHandler<TEvent extends keyof TEventToListenerArgsMap>(
     eventType: TEvent,
   ): Listeners<TEventToListenerArgsMap[TEvent]> {
-    let handler: Listeners<TEventToListenerArgsMap[TEvent]> =
+    let handler: Listeners<TEventToListenerArgsMap[TEvent]> | undefined =
       this._listeners[eventType];
     if (!handler) {
       handler = this._listeners[eventType] = new Hook<Listener<TEventToListenerArgsMap[TEvent]>>();
@@ -90,4 +127,28 @@ export class Channel<TEventToListenerArgsMap extends BaseChannelEventType>
     // Then the event is passed to the next channel
     super.emit(eventType, ...rawArgs);
   }
+}
+
+export class PausableChannel<TEventToListenerArgsMap extends BaseChannelEventType> extends Channel<TEventToListenerArgsMap> {
+  private _paused: boolean = false;
+
+  pause(): void {
+    this._paused = true;
+  }
+  unpause(): void {
+    this._paused = false;
+  }
+
+  emit<
+    TEvent extends keyof TEventToListenerArgsMap,
+    TArgs extends TEventToListenerArgsMap[TEvent]
+  >(eventType: TEvent, ...rawArgs: TArgs): void {
+    if (this._paused) {
+      return;
+    }
+    
+    super.emit(eventType, ...rawArgs);
+  }
+
+
 }
