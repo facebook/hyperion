8a17c74a0e974fabd7fda3506502580a5c98fb3c
Phil Schwartz <philschwartz@fb.com>
2024-04-22 17:36:38 -0500

    Add hover publisher,  configurable from UI publisher - This adds an ability to configure `hover` from UI publisher, although there are some differences in that this publisher relies upon `mouseover` (hard reliance),  and `click` (maybe optionally if we want to relax that).

---
+++ packages/hyperion-autologging/src/ALHoverPublisher.ts
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+ */
+
+'use strict';
+
+import { ChannelEventType } from "@hyperion/hyperion-channel/src/Channel";
+import * as Types from "@hyperion/hyperion-util/src/Types";
+import * as ALEventIndex from "./ALEventIndex";
+import { ALSharedInitOptions } from "./ALType";
+import * as ALUIEventPublisher from "./ALUIEventPublisher";
+import { ALUIEventCaptureData } from "./ALUIEventPublisher";
+import { assert } from "@hyperion/hyperion-global";
+
+
+export type InitOptions = Types.Options<
+  Pick<ALUIEventPublisher.InitOptions, 'uiEvents'> &
+  Pick<ALSharedInitOptions<ChannelEventType<(ALUIEventPublisher.InitOptions)['channel']>>, 'channel'> & {
+    hoverDurationThresholdMS: number;
+  }
+>;
+
+export function publish(options: InitOptions): void {
+  const { channel, uiEvents, hoverDurationThresholdMS } = options;
+
+  const mouseOverConfig = uiEvents.find(e => e.eventName === 'mouseover');
+  assert(mouseOverConfig != null, 'mouseover event must be included in uiEvents config to enable hover.');
+  const clickConfig = uiEvents.find(e => e.eventName === 'click');
+  assert(clickConfig != null, 'click event must be included in uiEvents config to enable hover.');
+
+  let activeHover: ALUIEventCaptureData | null = null;
+  channel.addListener('al_ui_event_capture', eventData => {
+    if (eventData.event === 'mouseover') {
+      if (
+        // Mousing over a new target and the activeHovers targetElement
+        // is now the relatedTarget of the new event.
+        // For mouseover:
+        //    event.target (from hyperion, targetElement) – is the element where the mouse came over.
+        //    event.relatedTarget – is the element from which the mouse came (relatedTarget → target).
+        activeHover?.targetElement === eventData.domEvent?.relatedTarget
+      ) {
+        activeHover != null &&
+          maybeEmitEvent(activeHover, eventData, hoverDurationThresholdMS);
+      }
+      // Set new active hover
+      activeHover = eventData;
+    }
+    // Click should end active hover.
+    // If the click is on an interactable element that contains the hovered element, then finish
+    // the hover event, utilizing the click event timestamp
+    else if (
+      activeHover != null &&
+      eventData.event === 'click' &&
+      // Clicked element (could be exact target or interactable parent) contains the active hover element
+      eventData.element?.contains(activeHover.targetElement)
+    ) {
+      activeHover != null &&
+        maybeEmitEvent(activeHover, eventData, hoverDurationThresholdMS);
+      // Reset the active hover
+      activeHover = null;
+    }
+  });
+
+  function maybeEmitEvent(
+    startEventData: ALUIEventCaptureData,
+    leaveEventData: ALUIEventCaptureData,
+    hoverDurationThresholdMS: number,
+  ): void {
+    const hoverDuration =
+      leaveEventData.eventTimestamp - startEventData.eventTimestamp;
+    if (hoverDuration > hoverDurationThresholdMS) {
+      // Should refine, since it's only assigned from mouseover
+      const domEvent = startEventData.domEvent as MouseEvent;
+      channel.emit('al_ui_event',
+        {
+          ...startEventData,
+          metadata: {
+            ...startEventData.metadata,
+            hover_duration_ms: hoverDuration.toString(),
+          },
+          domEvent: domEvent,
+          eventIndex: ALEventIndex.getNextEventIndex(),
+          event: 'hover',
+        }
+      );
+    }
+  }
+}
