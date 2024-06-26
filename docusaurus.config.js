3495a317fedf506d0711ef2b1b13c7e55d4f58d0
Mehrdad Reshadi <mehrdad@reshadi.com>
2024-06-25 20:42:09 -0700

    Upgrade docusaurus and dependent packages
    Upgraded from v2 to v3.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -129,8 +130,9 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Meta Platforms, Inc. and affiliates. All Rights Reserved.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        additionalLanguages: ['bash', 'diff', 'json'],
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
     }),
 };
