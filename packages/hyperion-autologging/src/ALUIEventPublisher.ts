24fbd8311f6b4fdf0392dad49dcd31099481c1ba
Xiaoling Tang <xiaotang@meta.com>
2025-04-23 18:05:30 -0700

    v2:switch the logging order and ensure uiEventData take the precedence

--- packages/hyperion-autologging/src/ALUIEventPublisher.ts
+++ packages/hyperion-autologging/src/ALUIEventPublisher.ts
@@ -314,6 +314,7 @@ export function publish(options: InitOptions): void {
         surface,
         surfaceData,
         ...elementText,
+        metadata: {...surfaceData?.metadata,...uiEventData.metadata},
         reactComponentName: reactComponentData?.name,
         reactComponentStack: reactComponentData?.stack,
       };
