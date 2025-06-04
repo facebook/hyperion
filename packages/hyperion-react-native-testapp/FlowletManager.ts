ffc8776a6ba0537c27ff5afd84d6536f52693fa1
Phil Schwartz <philschwartz@fb.com>
2025-06-04 11:23:39 -0500

    init

---
+++ packages/hyperion-react-native-testapp/FlowletManager.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+ */
+
+import { Flowlet } from "hyperion-flowlet/src/Flowlet";
+import { ALFlowletManager, ALFlowletDataType } from "hyperion-autologging/src/ALFlowletManager";
+
+interface FlowletDataType extends ALFlowletDataType {
+  i?: number
+}
+
+export const FlowletManager = new ALFlowletManager<FlowletDataType>();
+
+FlowletManager.push(new Flowlet("top", FlowletManager.top()));
