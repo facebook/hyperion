9e36a9fea49742b1b02e759afc03f5e8c32c6d95
Mehrdad Reshadi <mehrdad@reshadi.com>
2025-08-15 12:49:47 -0700

    Example for chaining fetches
    To test if the trigger flowlet is correct, updated the example to make a
    chain of calls using fetch, promise, timeout that stresses multiple
    paths for calculating trigger flowlet.
    
    This already shows that the logic is not fully robust. (will address
    next)

--- packages/hyperion-react-testapp/src/component/DynamicSvgComponent.tsx
+++ packages/hyperion-react-testapp/src/component/DynamicSvgComponent.tsx
@@ -5,6 +5,7 @@
 import * as React from "react";
 import { FlowletManager } from "../FlowletManager";
 import { Surface } from "./Surface";
+import { link } from "fs";
 
 function ResultViewer(props: { text: string }) {
   const ref = React.useRef<HTMLDivElement>(null);
@@ -14,7 +15,7 @@ function ResultViewer(props: { text: string }) {
     }
   });
 
-  return Surface({ surface: 'result', capability:{trackVisibilityThreshold: .5} })(
+  return Surface({ surface: 'result', capability: { trackVisibilityThreshold: .5 } })(
     <div ref={ref} style={{ width: "100px" }}></div>
   );
 }
@@ -27,36 +28,71 @@ export default function (/* props: Props */) {
   }, []);
 
   const links = [
+    // "robots.txt?a=1",
+    "observability.svg",
+    "Meta_Platforms_Inc_logo.svg",
     "https://hyperionjs.com/img/hyperion.svg",
-    "robots.txt?a=1",
+    // "https://hyperionjs.com/img/favicon.ico",
     // "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/facebook.svg",
     // "https://www.svgrepo.com/show/4733/samples.svg",
   ];
 
   const onCallbackFetch = React.useCallback(() => {
     // setText("Loading via fetch ..."); // Note if call these now, the corresponding component will be mounted. We want to mount alap to test the alflowlet flow
-    for (const link of links) {
-      fetch(link).then(response => response.text()).then(text => {
-        console.log("Fetch result: ", FlowletManager.top());
-        setText(text);
-      });
+    const iterator = links[Symbol.iterator]();
+    function fetchNext() {
+      const next = iterator.next();
+      if (next.done) {
+        return;
+      } else {
+        const link = next.value;
+        console.log("Fetching link: ", link, FlowletManager.top());
+        fetch(link).then(response => response.text()).then(text => {
+          console.log("Fetch results for ", link, FlowletManager.top());
+          setText(text);
+          setTimeout(fetchNext, 1000); // Wait a bit before fetching the next link
+          // fetchNext();
+        }
+        ).catch(err => {
+          console.error("Fetch error: ", err);
+        });
+      }
     }
-  }, []);
+
+    // Start the fetch process
+    fetchNext();
+  }, [links]);
 
   const onCallbackXHR = React.useCallback(() => {
     // setText("Loading via xhr ..."); // Note if call these now, the corresponding component will be mounted. We want to mount alap to test the alflowlet flow
-    for (const link of links) {
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", link);
-      xhr.addEventListener("loadend", ev => {
-        setText(xhr.responseText);
-      });
-      xhr.send();
+    const iterator = links[Symbol.iterator]();
+    function fetchNext() {
+      const next = iterator.next();
+      if (next.done) {
+        return;
+      } else {
+        const link = next.value;
+        console.log("Fetching xhr link: ", link, FlowletManager.top());
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", link);
+        xhr.addEventListener("loadend", ev => {
+          console.log("XHR results for ", link, FlowletManager.top());
+          setText(xhr.responseText);
+          setTimeout(fetchNext, 1000); // Wait a bit before fetching the next link
+        });
+        xhr.addEventListener("error", ev => {
+          console.error("XHR error: ", ev);
+        });
+        xhr.send();
+      }
     }
-  }, []);
+
+    // Start the fetch process
+    fetchNext();
+  }, [links]);
 
   return Surface({ surface: "loader" })(
-    <table border={1}>
+    <table border={1} width={"100%"}>
       <tbody>
         <tr>
           <td>
