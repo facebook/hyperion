fe553f2095b89ecdee9dd233a3ad4e3bf8d169e0
Marcos Bustamante <mbustamante@meta.com>
2025-09-01 19:01:12 +0100

    [rn] Creating a smaller build to be used in react native applications

---
+++ src/mobile/index.ts
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+ */
+
+'use strict';
+
+// hyperionGlobal
+// export * from "hyperion-globals/src/index";
+
+// hyperionAsyncCounter
+// export * from "hyperion-async-counter/src/index";
+
+// hyperionHook
+// export * from "hyperion-hook/src/index";
+
+// hyperionChannel
+// export * from "hyperion-channel/src/index";
+
+// hyperionTimedTrigger
+// export * from  "hyperion-timed-trigger/src/index";
+
+// hyperionTestAndSet
+export * from "hyperion-test-and-set/src/index";
+
+// hyperionCore
+export * from "hyperion-core/src/index";
+
+// hyperionDOM
+// export * from "hyperion-dom/src/index";
+
+// hyperionTrackElementsWithAttributes
+// export { trackElementsWithAttributes } from "hyperion-util/src/trackElementsWithAttributes";
+
+// hyperionSyncMutationObserver
+// export * as SyncMutationObserver from "hyperion-util/src/SyncMutationObserver";
+
+// hyperionUtil
+// export * from "hyperion-util/src/index";
+
+// hyperionFlowletCore
+// export { Flowlet, onFlowletInit } from "hyperion-flowlet/src/Flowlet";
+// export { FlowletManager } from "hyperion-flowlet/src/FlowletManager";
+// export { getTriggerFlowlet, setTriggerFlowlet } from "hyperion-flowlet/src/TriggerFlowlet";
+
+// hyperionFlowlet
+// export { initFlowletTrackers } from "hyperion-flowlet/src/FlowletWrappers";
+// export * from "hyperion-flowlet/src/index";
+
+// hyperionReact
+export *  from "hyperion-react/src/index";
+
+// hyperionAutoLogging
+// export *  from "hyperion-autologging/src/index";
+
+// hyperionAutoLoggingVisualizer
+// export * from "hyperion-autologging-visualizer/src/index";
+
+// hyperionAutoLoggingPluginEventHash
+// export * from "hyperion-autologging-plugin-eventhash/src/index";
